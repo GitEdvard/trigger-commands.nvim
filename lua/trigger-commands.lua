f56e9a1fb0d425e788305891055c7b7c648ae2b4
Edvard Englund <edvard.englund@medsci.uu.se>
2023-01-06 12:35:09 +0100

    Refactor, create functions for on_ events

--- lua/trigger-commands.lua
+++ lua/trigger-commands.lua
@@ -70,10 +70,7 @@ M._generate_command = function(run_settings)
     return command
 end
 
-M.run = function(run_settings)
-    if run_settings == nil or next(run_settings) == nil then
-        error('run settings is empty')
-    end
+local spawn_scratch_window = function()
     local original_win = vim.api.nvim_get_current_win()
     vim.cmd.vnew()
     -- Make it a scratch buffer
@@ -83,38 +80,57 @@ M.run = function(run_settings)
     vim.cmd{cmd = "setlocal", args = {"noswapfile"}}
     local bufnr = vim.api.nvim_get_current_buf()
     local prompt_win = vim.api.nvim_get_current_win()
-    vim.api.nvim_set_current_win(original_win)
+    -- vim.api.nvim_set_current_win(original_win)
+    return bufnr, prompt_win
+end
+
+local show_and_gather_err = function(data, err_output, bufnr, prompt_win)
+    if not data then
+        return err_output
+    end
+    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    move_cursor(prompt_win, #data)
+    for _, row in ipairs(data) do
+        table.insert(err_output, row)
+    end
+    return err_output
+end
+
+local show = function(data, bufnr, prompt_win)
+    if not data then
+        return
+    end
+    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+    move_cursor(prompt_win, #data)
+end
+
+local show_errors = function(exit_code, err_output, bufnr, prompt_win)
+    if exit_code ~= 0 then
+        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, {"Program returned error. Output written to the quickfix."})
+        move_cursor(prompt_win, 1)
+        local vim_script_arr = to_vim_script_arr(err_output)
+        vim.cmd { cmd = 'cgetexpr', args = {vim_script_arr} }
+    end
+end
+
+M.run_single = function(run_settings)
+    if run_settings == nil or next(run_settings) == nil then
+        error('run settings is empty')
+    end
+    local bufnr, prompt_win = spawn_scratch_window()
     local command = M._generate_command(run_settings)
     local err_output = {}
     vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "Waiting for script output ..."})
     vim.fn.jobstart(command, {
         stdout_buffered = true,
         on_stdout = function(_, data)
-            if not data then
-                return
-            end
-            vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-            move_cursor(prompt_win, #data)
+            show(data, bufnr, prompt_win)
         end,
         on_stderr = function(_, data)
-            if not data then
-                return
-            end
-            -- local data_vim_arr = to_vim_script_arr(data)
-            vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-            for _, row in ipairs(data) do
-                table.insert(err_output, row)
-            end
-            move_cursor(prompt_win, #data)
+            err_output = show_and_gather_err(data, err_output, bufnr, prompt_win)
         end,
         on_exit = function(_, exit_code, _)
-            if exit_code ~= 0 then
-                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, {"Program returned error. Output written to the quickfix."})
-                move_cursor(prompt_win, 1)
-                local vim_script_arr = to_vim_script_arr(err_output)
-                vim.cmd { cmd = 'cgetexpr', args = {vim_script_arr} }
-            end
-
+            show_errors(exit_code, err_output, bufnr, prompt_win)
         end
     })
 end
