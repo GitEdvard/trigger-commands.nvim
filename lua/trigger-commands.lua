bc99e64087c9d7b26816edd811986de8f1d63643
Edvard Englund <edvard.englund_c@sandvik.com>
2023-08-29 17:02:15 +0200

    Implement run_multi

--- lua/trigger-commands.lua
+++ lua/trigger-commands.lua
@@ -133,24 +133,52 @@ local has_errors = function(err_output)
     return false
 end
 
+jobstart_rec = function(commands, i, err_output, bufnr, prompt_win)
+  vim.fn.jobstart(commands[i], {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      show(data, bufnr, prompt_win)
+    end,
+    on_stderr = function(_, data)
+      local err_output = show_and_gather_err(data, err_output, bufnr, prompt_win)
+    end,
+    on_exit = function(_, exit_code, _)
+      if i < #commands then
+        jobstart_rec(commands, i + 1, err_output, bufnr, prompt_win)
+      else
+        show({"Done."}, bufnr, prompt_win)
+      end
+      if exit_code ~= 0 then
+      end
+    end
+  })
+end
+
+M.run_multi = function(commands)
+  local bufnr, prompt_win = spawn_scratch_window()
+  local err_output = {}
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "Waiting for script output ..."})
+  jobstart_rec(commands, 1, err_output, bufnr, prompt_win)
+end
+
 M.run_single = function(command)
-    local bufnr, prompt_win = spawn_scratch_window()
-    local err_output = {}
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "Waiting for script output ..."})
-    vim.fn.jobstart(command, {
-        stdout_buffered = true,
-        on_stdout = function(_, data)
-            show(data, bufnr, prompt_win)
-        end,
-        on_stderr = function(_, data)
-            err_output = show_and_gather_err(data, err_output, bufnr, prompt_win)
-        end,
-        on_exit = function(_, exit_code, _)
-            if exit_code ~= 0 then
-                show_errors(err_output, bufnr, prompt_win)
-            end
-        end
-    })
+  local bufnr, prompt_win = spawn_scratch_window()
+  local err_output = {}
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "Waiting for script output ..."})
+  vim.fn.jobstart(command, {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      show(data, bufnr, prompt_win)
+    end,
+    on_stderr = function(_, data)
+      err_output = show_and_gather_err(data, err_output, bufnr, prompt_win)
+    end,
+    on_exit = function(_, exit_code, _)
+      if exit_code ~= 0 then
+        show_errors(err_output, bufnr, prompt_win)
+      end
+    end
+  })
 end
 
 local generate_rest_command = function(command)
