de417e021b88379966ebd2e7bce2ab0110d18fd6
Edvard Englund <edvard.englund_c@sandvik.com>
2023-09-09 11:52:53 +0200

    Add run_silent

--- lua/trigger-commands.lua
+++ lua/trigger-commands.lua
@@ -12,4 +12,8 @@ M.run_rest_call = function(run_settings)
   require'rest-commands'.run_rest_call(run_settings)
 end
 
+M.run_silent = function(input)
+  require'silent-commands'.run_silent(input)
+end
+
 return M
